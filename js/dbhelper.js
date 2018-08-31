99cc1a630f55d42c8cce6e9c9b1e73759102eec4
ParthS007 <parth1989shandilya@gmail.com>
2018-09-01 00:44:49 +0530

    Implemented Fetch API usage

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -3,25 +3,21 @@ class DBHelper {
 
   /*Database URL -> Change this to restaurants.json file location on your server.*/
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /*Fetch all restaurants.*/
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+    fetch(DBHelper.DATABASE_URL)
+      .then(response => {
+        return response.json();
+      }).then(restaurants => {
         callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+      }).catch((error) => {
+        const errorMessage = (`Request failed. Error Message: ${error}`);
+        callback(errorMessage, null);
+      });
   }
 
   /*Fetch a restaurant by its ID.*/
@@ -127,16 +123,17 @@ class DBHelper {
 
   /*Restaurant image URL.*/
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /*Map marker for a restaurant.*/
    static mapMarkerForRestaurant(restaurant, map) {
     // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
-      {title: restaurant.name,
-      alt: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant)
+      {
+        title: restaurant.name,
+        alt: restaurant.name,
+        url: DBHelper.urlForRestaurant(restaurant)
       })
       marker.addTo(newMap);
     return marker;
